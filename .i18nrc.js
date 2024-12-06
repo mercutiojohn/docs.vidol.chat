02f23f999f359cc48ce3158ffe2d871c6f4f283c
rdmclin2 <rdmclin2@gmail.com>
2024-12-06 18:33:11 +0800

    chore: update zh-CN docs

--- .i18nrc.js
+++ .i18nrc.js
@@ -10,8 +10,7 @@ module.exports = defineConfig({
   reference:
     "You need to maintain the component format of MDX, and the output text does not need to be wrapped in any code block syntax at the outermost level.",
   markdown: {
-    entry: ["./docs/**/*.mdx"],
-    entryExtension: ".mdx",
+    entry: ["./docs/**/*.zh-CN.mdx"],
     entryLocale: "zh-CN",
     outputLocales: ["en-US", "ja-JP", "fr-FR"],
     exclude: [],
